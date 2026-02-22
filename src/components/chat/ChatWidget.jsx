7969bc96b11356e4ce122e801ca91c3f6d231fc0
danendral <dlohanata@gmail.com>
2026-02-22 16:23:24 +0700

    Commit

--- src/components/chat/ChatWidget.jsx
+++ src/components/chat/ChatWidget.jsx
@@ -100,8 +100,7 @@ export default function ChatWidget() {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           sessionId,
-          message: trimmed,
-          channel: 'web'
+          message: trimmed
         }),
       })
 
